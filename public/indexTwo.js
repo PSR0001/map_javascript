f736efb9ffa91b602a63a9cf7107c6d0d5545ff7
Partha <parthasingharoy166@gmail.com>
2021-11-24 16:23:53 +0530

    add two js file for front page

---
+++ public/indexTwo.js
@@ -0,0 +1,11 @@
+let submit= document.getElementById('submit');
+let box=document.getElementById('container')
+submit.addEventListener('click', ()=>{
+
+    document.getElementById('submit').style.background = 'red';
+})
+
+console.log(box)
+
+
+
